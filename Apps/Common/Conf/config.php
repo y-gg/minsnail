2f5f28fd44bca7b4f1e611d751959a7e89b51d4f
yg <yg2010.6.11@qq.com>
2016-09-15 16:39:53 +0800

    modifiled config.php

--- Apps/Common/Conf/config.php
+++ Apps/Common/Conf/config.php
@@ -3,13 +3,13 @@ return array(
 	//'配置项'=>'配置值'
 	//数据库配置信息
 'DB_TYPE'   => 'mysql', // 数据库类型
-'DB_HOST'   => '', // 服务器地址
-'DB_NAME'   => '', // 数据库名
-'DB_USER'   => '', // 用户名
-'DB_PWD'    => '', // 密码
+'DB_HOST'   => 'mysql-m', // 服务器地址
+'DB_NAME'   => 'm2753078_minsnail', // 数据库名
+'DB_USER'   => 'm2753078admin', // 用户名
+'DB_PWD'    => '13085138', // 密码
 'DB_PORT'   => 3306, // 端口
 'DB_PARAMS' =>  array(), // 数据库连接参数
-'DB_PREFIX' => '', // 数据库表前缀 
+'DB_PREFIX' => 'minsnail_', // 数据库表前缀 
 'DB_CHARSET'=> 'utf8', // 字符集
 'DB_DEBUG'  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
 );
